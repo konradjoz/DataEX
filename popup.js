d2d64953ebf942a0a8aacd6d114afa89d3b566e1
Konrad <konradjozefczyk2003@gmail.com>
2022-05-16 21:13:06 +0200

    Foundation

---
+++ popup.js
@@ -0,0 +1,18 @@
+// When the button is clicked, inject setPageBackgroundColor into current page
+clipboard.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    $("#copy-clipboard-1").css("", color)
+    document.body.style.backgroundColor = color;
+  });
+}
